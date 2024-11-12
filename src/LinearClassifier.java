62c6bbff40bf5466a6d6785210da86f4196ee882
Chris <59696583+Chris0Jeky@users.noreply.github.com>
2024-11-12 12:34:00 +0000

    initial data

---
+++ src/LinearClassifier.java
@@ -0,0 +1,15 @@
+public class LinearClassifier {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        double[][] points = {
+                {2.0, 3.0, 0},
+                {1.0, 1.5, 0},
+                {4.0, 5.0, 1},
+                {6.0, 7.5, 1}
+        };
+
+        
+
+    }
+}
