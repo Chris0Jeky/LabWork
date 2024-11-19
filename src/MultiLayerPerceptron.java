ba984a7e0188550a28d83932485251c3c3c5cf21
Chris <59696583+Chris0Jeky@users.noreply.github.com>
2024-11-19 12:35:21 +0000

    Building a Multi-Layer Perceptron (Two Hidden Neurons, One Output Neuron)

--- src/MultiLayerPerceptron.java
+++ src/MultiLayerPerceptron.java
@@ -1,33 +1,31 @@
 public class MultiLayerPerceptron {
 
-    // First perceptron
-    double w1_p1 = 0.5;
-    double w2_p1 = 0.5;
-    double bias_p1 = -0.5;
+    // Weights for input to hidden layer
+    double[][] weights_input_hidden = {
+            {0.5, 0.5},   // Weights for first hidden neuron
+            {0.5, -0.5}   // Weights for second hidden neuron
+    };
+    double[] bias_hidden = {-0.5, 0.5}; // Biases for hidden neurons
 
-    // Second perceptron
-    double w1_p2 = 1.0;
-    double w2_p2 = -1.0;
-    double bias_p2 = 0.5;
+    // Weights for hidden to output layer
+    double[] weights_hidden_output = {1.0, 1.0}; // Weights for output neuron
+    double bias_output = -1.0; // Bias for output neuron
 
-    // Third perceptron
-    double w1_p3 = -1.0;
-    double w2_p3 = 1.0;
-    double bias_p3 = 0.5;
+    public int multiLayerPerceptron(double input1, double input2) {
+        // Hidden layer computations
+        double[] hidden_outputs = new double[2];
+        for (int i = 0; i < 2; i++) {
+            double sum = (input1 * weights_input_hidden[i][0]) + (input2 * weights_input_hidden[i][1]) + bias_hidden[i];
+            hidden_outputs[i] = stepFunction(sum);
+        }
 
-    public int perceptron1(double input1, double input2) {
-        double sum = (input1 * w1_p1) + (input2 * w2_p1) + bias_p1;
-        return stepFunction(sum);
-    }
+        // Output layer computation
+        double sum_output = (hidden_outputs[0] * weights_hidden_output[0]) +
+                (hidden_outputs[1] * weights_hidden_output[1]) +
+                bias_output;
+        int output = stepFunction(sum_output);
 
-    public int perceptron2(double input1, double input2) {
-        double sum = (input1 * w1_p2) + (input2 * w2_p2) + bias_p2;
-        return stepFunction(sum);
-    }
-
-    public int perceptron3(double input1, double input2) {
-        double sum = (input1 * w1_p3) + (input2 * w2_p3) + bias_p3;
-        return stepFunction(sum);
+        return output;
     }
 
     public int stepFunction(double sum) {
@@ -36,15 +34,10 @@ public class MultiLayerPerceptron {
 
     public static void main(String[] args) {
         MultiLayerPerceptron mlp = new MultiLayerPerceptron();
-        double input1 = 1.0;
-        double input2 = 2.0;
+        double input1 = 0.0;
+        double input2 = 1.0;
 
-        int output1 = mlp.perceptron1(input1, input2);
-        int output2 = mlp.perceptron2(input1, input2);
-        int output3 = mlp.perceptron3(input1, input2);
-
-        System.out.println("Output of Perceptron 1: " + output1);
-        System.out.println("Output of Perceptron 2: " + output2);
-        System.out.println("Output of Perceptron 3: " + output3);
+        int output = mlp.multiLayerPerceptron(input1, input2);
+        System.out.println("Output: " + output);
     }
 }
