49f367df04920192e5dffbb9fe6a83cd82bef55b
Chris <59696583+Chris0Jeky@users.noreply.github.com>
2024-11-19 12:29:55 +0000

    added two more perceptrons

--- src/MultiLayerPerceptron.java
+++ src/MultiLayerPerceptron.java
@@ -1,23 +1,50 @@
 public class MultiLayerPerceptron {
 
-    double weight1 = 0.5;
-    double weight2 = 0.5;
-    double bias = 0.5;
+    // First perceptron
+    double w1_p1 = 0.5;
+    double w2_p1 = 0.5;
+    double bias_p1 = -0.5;
+
+    // Second perceptron
+    double w1_p2 = 1.0;
+    double w2_p2 = -1.0;
+    double bias_p2 = 0.5;
+
+    // Third perceptron
+    double w1_p3 = -1.0;
+    double w2_p3 = 1.0;
+    double bias_p3 = 0.5;
+
+    public int perceptron1(double input1, double input2) {
+        double sum = (input1 * w1_p1) + (input2 * w2_p1) + bias_p1;
+        return stepFunction(sum);
+    }
+
+    public int perceptron2(double input1, double input2) {
+        double sum = (input1 * w1_p2) + (input2 * w2_p2) + bias_p2;
+        return stepFunction(sum);
+    }
+
+    public int perceptron3(double input1, double input2) {
+        double sum = (input1 * w1_p3) + (input2 * w2_p3) + bias_p3;
+        return stepFunction(sum);
+    }
 
     public int stepFunction(double sum) {
         return (sum >= 0) ? 1 : 0;
     }
 
-    // Perceptron with two inputs
-    public double perceptron(double input1, double input2) {
-        // Linear transfer function: output = input
-        double sum = (input1 * weight1) + (input2 * weight2) + bias;
-        return stepFunction(sum); // Pass-through function
-    }
-
     public static void main(String[] args) {
         MultiLayerPerceptron mlp = new MultiLayerPerceptron();
-        double output = mlp.perceptron(1.0, 2.0);
-        System.out.println("Output: " + output);
+        double input1 = 1.0;
+        double input2 = 2.0;
+
+        int output1 = mlp.perceptron1(input1, input2);
+        int output2 = mlp.perceptron2(input1, input2);
+        int output3 = mlp.perceptron3(input1, input2);
+
+        System.out.println("Output of Perceptron 1: " + output1);
+        System.out.println("Output of Perceptron 2: " + output2);
+        System.out.println("Output of Perceptron 3: " + output3);
     }
 }
