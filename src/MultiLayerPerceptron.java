e62d107c737011ca8325c229dce3ded69cb1cf22
Chris <59696583+Chris0Jeky@users.noreply.github.com>
2024-11-19 12:27:10 +0000

    added bias and stepFunction

--- src/MultiLayerPerceptron.java
+++ src/MultiLayerPerceptron.java
@@ -2,12 +2,17 @@ public class MultiLayerPerceptron {
 
     double weight1 = 0.5;
     double weight2 = 0.5;
+    double bias = 0.5;
+
+    public int stepFunction(double sum) {
+        return (sum >= 0) ? 1 : 0;
+    }
 
     // Perceptron with two inputs
     public double perceptron(double input1, double input2) {
         // Linear transfer function: output = input
-        double sum = (input1 * weight1) + (input2 * weight2);
-        return sum; // Pass-through function
+        double sum = (input1 * weight1) + (input2 * weight2) + bias;
+        return stepFunction(sum); // Pass-through function
     }
 
     public static void main(String[] args) {
