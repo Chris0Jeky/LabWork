37fcbe4ac747f9924d5ae13a034c9e24bbfeba1c
Chris0Jeky <jeky.tck@gmail.com>
2024-12-09 00:52:08 +0000

    working basic euclidean distance

---
+++ src/EuclideanDistance.java
@@ -0,0 +1,33 @@
+public class EuclideanDistance {
+
+    public static void main(String[] args) {
+        System.out.println("Euclidean Distance");
+
+        int[][] cities = {{1, 1}, {5, 5}, {10, 3}, {2, 7}};
+
+        print2DArrays(cities);
+
+        System.out.println("Distance between city 1 and city 2: " + euclideanDistance(cities[0], cities[1]));
+    }
+
+    public static void print2DArrays(int[][] array) {
+        System.out.println("Printing 2D Array:");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print("City " + (i + 1) + ": ");
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static float euclideanDistance(int[] a, int[] b) {
+        // Validate input arrays
+        if (a.length != 2 || b.length != 2) {
+            throw new IllegalArgumentException("Both input arrays must have exactly 2 elements.");
+        }
+
+        // Compute the Euclidean distance
+        return (float) Math.sqrt(Math.pow((a[0] - b[0]), 2) + Math.pow((a[1] - b[1]), 2));
+    }
+}
