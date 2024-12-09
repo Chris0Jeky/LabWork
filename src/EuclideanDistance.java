c6d6ec24acd88d3e5bd799a178bf92cec56b8f3b
Chris0Jeky <jeky.tck@gmail.com>
2024-12-09 03:24:07 +0000

    working permutations solution

--- src/EuclideanDistance.java
+++ src/EuclideanDistance.java
@@ -1,16 +1,52 @@
+import java.util.Arrays;
+
 public class EuclideanDistance {
 
     public static void main(String[] args) {
         System.out.println("Euclidean Distance");
 
         int[][] cities = {{1, 1}, {5, 5}, {10, 3}, {2, 7}};
-        int[] sequence = new int[cities.length];
+        int[] initialSequence = new int[cities.length];
+        for (int i = 0; i < cities.length; i++) {
+            initialSequence[i] = i;
+        }
+        permute(cities, initialSequence, 0);
 
 
         print2DArrays(cities);
 
         System.out.println("Distance between city 1 and city 2: " + euclideanDistance(cities[0], cities[1]));
-        System.out.println(calculatePermuations(cities, sequence, false));
+
+
+    }
+
+    static float bestDistance = Float.MAX_VALUE;
+    static int[] bestSequence;
+
+    private static void permute(int[][] cities, int[] sequence, int start) {
+        if (start == sequence.length - 1) {
+            float dist = totalDistance(cities, sequence);
+            System.out.println("Permutation: " + Arrays.toString(sequence) + " Distance: " + dist);
+            if (dist < bestDistance) {
+                bestSequence = sequence.clone();
+                bestDistance = dist;
+            }
+            return;
+        }
+
+        for (int i = start; i < sequence.length; i++){
+            swap(sequence, start, i);
+            permute(cities, sequence, start + 1);
+            swap(sequence, start, i);
+        }
+    }
+
+    private static float totalDistance(int[][] cities, int[] sequence) {
+        float dist = 0f;
+        for (int i = 0; i < sequence.length - 1; i++) {
+            dist += euclideanDistance(cities[sequence[i]], cities[sequence[i + 1]]);
+        }
+        return dist;
     }
 
     public static void print2DArrays(int[][] array) {
@@ -34,19 +70,7 @@ public class EuclideanDistance {
         return (float) Math.sqrt(Math.pow((a[0] - b[0]), 2) + Math.pow((a[1] - b[1]), 2));
     }
 
-    public static int[] calculatePermuations(int[][] array, int[] sequence, boolean stop) {
-        boolean yes = true;
-        System.out.println("Calculating permuations");
-        System.out.println("stop = " + stop);
-        if (!stop){
-            if (yes){
-                stop = true;
-                System.out.println("stop = true now");
-            }
-            calculatePermuations(array, sequence, stop);
-        }
-        return sequence;
-    }
+
 
     public static int[] swap(int[] array, int i, int j) {
         int temp = array[i];
